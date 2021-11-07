bcdc5c9677b89b2efa0bd57e5eb02ce8edfd2656
bsao17 <declic62@gmail.com>
2021-11-07 19:14:25 +0100

    Create mysql connection

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const port = 3000;
-
-app.use(express.static('public'));
-app.use(express.json());
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-mysql.createConnection({
+const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'test'
+});
+
+app.use(express.static('public'));
+app.use(express.json());
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
