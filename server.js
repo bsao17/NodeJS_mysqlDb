0ddd2d7ff18fb9a92d1a29f89dac6c430a493713
bsao17 <declic62@gmail.com>
2021-11-07 19:21:14 +0100

    run server and create database test

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'test'
 });
 
 app.use(express.static('public'));
@@ -18,5 +17,31 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 });
+con.query("CREATE DATABASE test", (err, result) => {
+    if (err) throw err;
+    console.log("Database created");
+    });
+con.query("CREATE TABLE test.users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), password VARCHAR(255))", (err, result) => {
+    if (err) throw err;
+    console.log("Table created");
+    });
+app.post('/api/users', (req, res) => {
+    const { name, email, password } = req.body;
+    const sql = `INSERT INTO test.users (name, email, password) VALUES ('${name}', '${email}', '${password}')`;
+    con.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("1 record inserted");
+        res.send(result);
+    });
+});
+app.get('/api/users', (req, res) => {
+    const sql = 'SELECT * FROM test.users';
+    con.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("Data received");
+        res.send(result);
+    });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
